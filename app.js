3570af4680655aacf0e5486de3b190f8e8405c85
MityaDementiy <d.dementiy@gmail.com>
2023-11-26 13:10:41 +0200

    feat: enable authentication

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import passport from 'passport';
+import BearerStrategy from 'passport-http-bearer';
 
 import connectDB from './config/db.js';
 import usersRouter from './src/routes/users.js';
+import User from './src/models/user.js';
 
 const app = express();
 
 dotenv.config();
 
 const port = process.env.PORT ?? 8080;
+
+passport.use(new BearerStrategy(
+  async (token, done) => {
+    try {
+      const user = await User.findOne({ token });
+
+      if (!user) {
+        return done(null, false);
+      }
+
+      return done(null, user, { scope: 'all' });
+    } catch (error) {
+      return done(error);
+    }
+  },
+));
+
+app.use(passport.initialize());
 
 app.use('/api', cors());
 
@@ -18,7 +39,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.json());
-app.use('/api', usersRouter);
+app.use('/api', passport.authenticate('bearer', { session: false }), usersRouter);
 
 connectDB();
 
